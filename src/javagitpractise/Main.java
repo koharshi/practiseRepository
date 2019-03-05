ad45eb9cb7a9d97339307f33b61ed1a7b23929ad
Harshit-Kohli-Sheridan <kohlih17@gmail.com>
2019-03-05 03:32:57 -0500

    new line

--- src/javagitpractise/Main.java
+++ src/javagitpractise/Main.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javagitpractise;
 
-/**
- *
- * @author harsh and kabir
- */
 public class Main {
 
     /**
@@ -19,6 +11,8 @@ public class Main {
         System.out.println("my name is harshit kohli ");
         
         System.out.println("The green light on sides which show up when u add new code tells you that this code is different from ehat you have saved on the git hub ");
+        
+        System.out.println("new things to play with this is branch kohli");
     }
     
     

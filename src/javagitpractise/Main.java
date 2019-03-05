b566e0fae2b3e7596a42f7da3e1a46afb3388271
Harshit-Kohli-Sheridan <kohlih17@gmail.com>
2019-03-05 03:30:22 -0500

    added name things to play with

--- src/javagitpractise/Main.java
+++ src/javagitpractise/Main.java
@@ -19,6 +19,8 @@ public class Main {
         System.out.println("my name is harshit kohli ");
         
         System.out.println("The green light on sides which show up when u add new code tells you that this code is different from ehat you have saved on the git hub ");
+        
+        System.out.println("new things to play with this is branch kohli");
     }
     
     
